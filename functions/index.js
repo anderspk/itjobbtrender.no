eda988a531d4e6664132292d14f7840df549af93
andy <kamsvaag.anders@gmail.com>
2020-05-10 14:28:06 +0200

    scheduled function debug

--- functions/index.js
+++ functions/index.js
@@ -5,11 +5,10 @@ const { finn } = require("./services/scrapers");
 exports.scheduledFunction = functions.pubsub
   // .schedule("0 10 * * *")
   // .timeZone("Europe/Oslo")
-  .schedule("every 5 minutes")
+  .schedule("every 15 minutes")
   .onRun(async (context) => {
     await finn();
     console.log("Scheduled Function Complete");
-    return null;
   });
 
 // exports.isolatedFunction = functions.https.onRequest(finn);

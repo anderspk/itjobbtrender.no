1a673a2bbe5668adc26a0c023a63d4dca3364cb4
anderspk <39174017+anderspk@users.noreply.github.com>
2020-11-25 14:17:41 +0100

    Update finnUtils.js

--- functions/services/utils/finnUtils.js
+++ functions/services/utils/finnUtils.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 
 const fetchData = async (url) => {
-  const result = await axios.get(url || SITE_URL);
+  const result = await axios.get(url);
   return cheerio.load(result.data);
 };
 
